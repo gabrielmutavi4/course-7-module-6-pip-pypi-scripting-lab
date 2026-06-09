80e17201348c3c4bb59f79d523e112225bc6a42b
gabrielmutavi4-GM <gabrielmutavi4@gmail.com>
2026-06-10 00:12:54 +0300

    update project files

--- lib/generate_log.py
+++ lib/generate_log.py
@@ -1,40 +1,14 @@
 from datetime import datetime
-import requests
 
+def generate_log(data):
 
-def generate_log():
-    log_data = [
-        "User logged in",
-        "User updated profile",
-        "Report exported"
-    ]
+    if not isinstance(data, list):
+        raise ValueError("Input must be a list.")
 
     filename = f"log_{datetime.now().strftime('%Y%m%d')}.txt"
 
     with open(filename, "w") as file:
-        for entry in log_data:
-            file.write(f"{entry}\n")
+        file.writelines(f"{entry}\n" for entry in data)
 
     print(f"Log written to {filename}")
-
-
-def fetch_data():
-    response = requests.get(
-        "https://jsonplaceholder.typicode.com/posts/1"
-    )
-
-    if response.status_code == 200:
-        return response.json()
-
-    return {}
-
-
-if __name__ == "__main__":
-    generate_log()
-
-    post = fetch_data()
-
-    print(
-        "Fetched Post Title:",
-        post.get("title", "No title found")
-    )
+    return filename
