0757d1ffb4a215cedf48c4b6623068711600ca08
gabrielmutavi4-GM <gabrielmutavi4@gmail.com>
2026-06-10 00:37:26 +0300

    fix project file errors

--- lib/generate_log.py
+++ lib/generate_log.py
@@ -3,12 +3,13 @@ from datetime import datetime
 def generate_log(data):
 
     if not isinstance(data, list):
-        raise ValueError("Input must be a list.")
+        raise ValueError("Input must be a list")
 
     filename = f"log_{datetime.now().strftime('%Y%m%d')}.txt"
 
     with open(filename, "w") as file:
-        file.writelines(f"{entry}\n" for entry in data)
+        for entry in data:
+            file.write(f"{entry}\n")
 
     print(f"Log written to {filename}")
     return filename
